902376c89555e382015818db8a996669d77ba06f
bawang <bawang@tibco.com>
2020-03-15 19:08:57 +0800

    try jqtree

---
+++ scripts/generate_github_index_data.py
@@ -0,0 +1,42 @@
+# https://stackoverflow.com/questions/11968976/list-files-only-in-the-current-directory
+
+import os
+import json
+
+#var data = 
+#    {
+#        name: 'node1', id: 1,
+#        children: [
+#            { name: 'child1', id: 2 },
+#            { name: 'child2', id: 3 }
+#        ]
+#    };
+def visit(d_or_f, dir_node, seq_dict):
+
+    if not os.path.isdir(d_or_f):
+        return
+    #print('visit:', d_or_f)
+
+    for f in os.listdir(d_or_f):
+        #print('visit:', f)
+
+        child = {'name':os.path.basename(f), 'id': (seq_dict['seq']), 'is_dir': os.path.isdir(f)}
+        seq_dict['seq'] = seq_dict['seq'] + 1
+        
+        if 'children' not in dir_node:
+            dir_node['children'] = []
+        dir_node['children'].append(child)
+
+
+        visit(f, child, seq_dict)
+
+
+root='../docs'
+seq_dict = {'seq':0}
+data = {'name':os.path.basename(root), 'id': seq_dict['seq']}
+seq_dict['seq'] = seq_dict['seq'] + 1
+
+visit(root, data, seq_dict)
+print(json.dumps(data))
+
+
