f4bb8eedf0298a87a1063c97d5d4b83608c9a84b
bawang <bawang@tibco.com>
2020-03-15 19:37:03 +0800

    fix index

--- docs/scripts/generate_github_index_data.py
+++ docs/scripts/generate_github_index_data.py
@@ -3,7 +3,9 @@
 import os
 import json
 
-#var data = 
+
+#
+# var data =
 #    {
 #        name: 'node1', id: 1,
 #        children: [
@@ -15,23 +17,23 @@ def visit(d_or_f, dir_node, seq_dict):
 
     if not os.path.isdir(d_or_f):
         return
-    #print('visit:', d_or_f)
+    # print('visit:', d_or_f)
 
-    for f in os.listdir(d_or_f):
-        #print('visit:', f)
+    # use scan instead of listdir, because listdir return 'string' but scandir return object
+    for f in os.scandir(d_or_f):
+        # print('visit:', f)
 
-        child = {'name':os.path.basename(f), 'id': (seq_dict['seq']), 'is_dir': os.path.isdir(f)}
+        child = {'name': os.path.basename(f), 'id': (seq_dict['seq']), 'is_dir': os.path.isdir(f)}
         seq_dict['seq'] = seq_dict['seq'] + 1
         
         if 'children' not in dir_node:
             dir_node['children'] = []
         dir_node['children'].append(child)
 
-
         visit(f, child, seq_dict)
 
 
-root='../md'
+root = '../md'
 seq_dict = {'seq':0}
 data = {'name':os.path.basename(root), 'id': seq_dict['seq']}
 seq_dict['seq'] = seq_dict['seq'] + 1
@@ -39,4 +41,4 @@ seq_dict['seq'] = seq_dict['seq'] + 1
 visit(root, data, seq_dict)
 print(json.dumps(data))
 
-
+# python3 generate_github_index_data.py| python3 -m json.tool
